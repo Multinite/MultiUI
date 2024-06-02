9220b2aa7263454f1344e71b57c5bcf3c0c773c0
Multinite <founders.multinite@gmail.com>
2024-06-03 00:12:06 +1000

    update

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,12 @@
 "use client";
+import { Input as RAC_Input } from "react-aria-components";
 
 function Input() {
-  return <div></div>;
+  return (
+    <div slot="inputWrapper" className="w-full ">
+      <RAC_Input />
+    </div>
+  );
 }
 
 export default Input;
