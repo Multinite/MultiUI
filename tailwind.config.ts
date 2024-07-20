ea0de6b0b5b7f9da0523fe1b56e2cc325aa60c4f
Multinite <founders.multinite@gmail.com>
2024-07-20 17:47:25 +1000

    modified tsconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
-import plugin from "tailwindcss/plugin";
+// import plugin from "tailwindcss/plugin";
+// import MultiUIPlugin from "./src/providers/MultiUIPlugin";
+// import type { RecursiveKeyValuePair } from "tailwindcss/types/config";
+
 import MultiUIPlugin from "./src/providers/MultiUIPlugin";
-import type { RecursiveKeyValuePair } from "tailwindcss/types/config";
+
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
