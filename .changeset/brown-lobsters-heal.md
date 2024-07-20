368393d98fe682094c9afcf4ecc51794801b11cc
Multinite <founders.multinite@gmail.com>
2024-07-21 07:29:50 +1000

    CLI update

---
+++ .changeset/brown-lobsters-heal.md
@@ -0,0 +1,5 @@
+---
+"@multinite_official/multiui": minor
+---
+
+CLI update
--- packages/multiui/src/cli/index.ts
+++ packages/multiui/src/cli/index.ts
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log("Hello from CLI!")
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("MultiUI")
+  .description("The MultiUI Component Library CLI.")
+  .version("v1.0.0");
+
+program
+  .command("add")
+  .description("Add a new component to your project.")
+  .argument("<name>", "name of the component")
+  .action((name) => {
+    console.log(`Adding ${name} component...`);
+  });
+
+program.parse();
+
