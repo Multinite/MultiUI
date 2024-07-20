fe2f62e07e2b66f174cf39c0c2c30aac3e0cf959
Multinite <founders.multinite@gmail.com>
2024-07-21 06:21:24 +1000

    Core package.json update

---
+++ .changeset/lazy-shrimps-taste.md
@@ -0,0 +1,9 @@
+---
+"@multinite_official/multiui-ui": patch
+"@multinite_official/multiui-docs": patch
+"@multinite_official/multiui-eslint-config": patch
+"@multinite_official/multiui": patch
+"@multinite_official/multiui-typescript-config": patch
+---
+
+Core package.json update
--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,10 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-    tsconfigRootDir: __dirname,
-  },
-};
+    root: true,
+    extends: ["@multinite_official/multiui-eslint-config/react-internal.js"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.lint.json",
+      tsconfigRootDir: __dirname,
+    },
+  };
+  
