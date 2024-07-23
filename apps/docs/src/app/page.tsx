1946ebb94e72bd76e1d3a33c1dec08ca37515b09
Multinite <founders.multinite@gmail.com>
2024-07-23 12:14:19 +1000

    Updated docs favicon

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@multinite_official/multiui/components";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center gap-5">
-      <Button>Hello</Button>
+      <Button className={``}>Hello</Button>
     </div>
   );
 }
