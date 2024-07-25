880eb89d1adb9a86151a4b3f7c47306081000e18
Multinite <founders.multinite@gmail.com>
2024-07-25 23:59:16 +1000

    Removed env file

--- apps/docs/drizzle.config.ts
+++ apps/docs/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: ".local.env" });
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
--- apps/docs/src/db/index.ts
+++ apps/docs/src/db/index.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-config({ path: ".local.env" });
+config({ path: ".env.local" });
 const client = postgres(process.env.DB_CONNECTION_URL!);
 export const db = drizzle(client);
