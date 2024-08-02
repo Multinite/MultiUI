114978e8d4118f50b3cf7672a9eed6c464adc278
Multinite <founders.multinite@gmail.com>
2024-08-02 12:49:09 +1000

    Updated: Default theme colors

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center gap-5" >
-      <span className="">Hello and welcome to the MultiUI docs!</span>
+    <div className="w-screen h-screen flex justify-center items-center gap-5 text-primary flex-col">
+      <span className="text-lg">Hello and welcome to the MultiUI docs!</span>
+      <span className="text-lg text-secondary">Hello and welcome to the MultiUI docs!</span>
     </div>
   );
 }
