297ba3b797a9cb81668f8afaaacc96d0854c65a7
Multinite <founders.multinite@gmail.com>
2024-08-03 23:41:06 +1000

    Fix: removed createComponent

--- packages/multiui/index.ts
+++ packages/multiui/index.ts
@@ -1,15 +1,5 @@
-import { createComponent } from "./providers/createComponent";
 import { cn, cn_seperator } from "./utils/cn";
-import type { CreateComponentProps } from "./providers/createComponent";
 import MultiUIPlugin from "./providers/MultiUIPlugin";
 import MultiUIProvider, { useTheme } from "./providers/MultiUIProvider";
 
-export {
-  createComponent,
-  cn,
-  cn_seperator,
-  MultiUIPlugin,
-  MultiUIProvider,
-  useTheme,
-};
-export type { CreateComponentProps };
+export { cn, cn_seperator, MultiUIPlugin, MultiUIProvider, useTheme };
