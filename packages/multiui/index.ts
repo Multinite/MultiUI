0bfea0c5f70b3813d783ea2dca1cbfe2bd469de0
Multinite <founders.multinite@gmail.com>
2024-08-03 23:43:58 +1000

    WIP Fix: Vercel can't build?

--- packages/multiui/index.ts
+++ packages/multiui/index.ts
@@ -1,5 +1,5 @@
-import { cn, cn_seperator } from "./utils/cn";
-import MultiUIPlugin from "./providers/MultiUIPlugin";
-import MultiUIProvider, { useTheme } from "./providers/MultiUIProvider";
+import { cn, cn_seperator } from "./utils/cn.js";
+import MultiUIPlugin from "./providers/MultiUIPlugin.js";
+import MultiUIProvider, { useTheme } from "./providers/MultiUIProvider.js";
 
 export { cn, cn_seperator, MultiUIPlugin, MultiUIProvider, useTheme };
