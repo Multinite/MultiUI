d9a6d72eccb75584025c5c4d6d2bf0d0ee6409ed
Multinite <founders.multinite@gmail.com>
2024-08-09 09:14:59 +1000

    Update: index export values

--- packages/multiui/index.ts
+++ packages/multiui/index.ts
@@ -1,5 +1,3 @@
-import { cn, cn_seperator } from "./utils/cn.js";
-import MultiUIPlugin from "./providers/MultiUIPlugin.js";
-import MultiUIProvider, { useTheme } from "./providers/MultiUIProvider.js";
-
-export { cn, cn_seperator, MultiUIPlugin, MultiUIProvider, useTheme };
+export { cn, cn_seperator } from "./utils/cn.js";
+export * from "./providers/MultiUIPlugin.js";
+export * from "./providers/MultiUIProvider.js";
