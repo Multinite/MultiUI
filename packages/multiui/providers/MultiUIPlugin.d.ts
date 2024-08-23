86a38b3553f92c979e93e200782f3dcad076908c
Multinite <founders.multinite@gmail.com>
2024-08-23 11:39:48 +1000

    Added: PackageLock for Pnpm

--- packages/multiui/providers/MultiUIPlugin.d.ts
+++ packages/multiui/providers/MultiUIPlugin.d.ts
@@ -2,6 +2,6 @@ import type { MultiUIConfig } from "./../types/MultiUIConfig.js";
 export declare const MultiUIPlugin: (multiUIConfig: MultiUIConfig & {
     $schema?: string;
 }) => {
-    handler: import("tailwindcss/types/config").PluginCreator;
-    config?: Partial<import("tailwindcss/types/config").Config>;
+    handler: import("tailwindcss/types/config.js").PluginCreator;
+    config?: Partial<import("tailwindcss/types/config.js").Config>;
 };
