238357534c728fc6616b2ebb0923f69db6118f15
Multinite <founders.multinite@gmail.com>
2024-10-12 22:31:43 +1000

    WIP: docs

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { useTheme } from "@multinite_official/multiui";
-import { dark } from "@clerk/themes";
+import Image from "next/image";
+import logo from "../../public/assets/logo_64.webp";
 
 export default function Home() {
   return (
@@ -16,9 +16,11 @@ export default function Home() {
 function Header() {
   return (
     <div className="w-screen h-32 py-10 grid grid-cols-[10%_80%_10%]">
-      <div className="pl-10"></div>
+      <div className="pl-10">
+        <Image src={logo} alt="MultiUI Logo" width={64} height={64} />
+      </div>
       <div className="flex items-center justify-center">
-        <header className="w-[50vw] h-full rounded-full border border-default bg-foreground/50 "></header>
+        <header className="w-[50vw] h-full rounded-full border border-default bg-foreground/50 bg-primary"></header>
       </div>
       <div className="flex items-center justify-center pr-10">
         <SignedIn>
